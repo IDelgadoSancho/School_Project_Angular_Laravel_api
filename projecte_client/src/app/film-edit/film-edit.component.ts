39630049b9580e78e695f18a91cc4b2bdf25779d
Ismael Delgado Sancho <ismael_delgadosancho@iescarlesvallbona.cat>
2025-02-11 17:21:59 +0100

    film edit done

--- projecte_client/src/app/film-edit/film-edit.component.ts
+++ projecte_client/src/app/film-edit/film-edit.component.ts
@@ -1,4 +1,13 @@
+import { HttpResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
+import { catchError, of } from 'rxjs';
+import { IDirector } from '../interfaces/idirector';
+import { DadesFilmsService } from '../services/dades-films.service';
+import { DadesDirectorsService } from '../services/dades-directors.service';
+import { title } from 'process';
 
 @Component({
   selector: 'app-film-edit',
@@ -7,5 +16,71 @@ import { Component } from '@angular/core';
   styleUrl: './film-edit.component.css'
 })
 export class FilmEditComponent {
+  id: string | null | undefined;
+  myForm: FormGroup;
+  errorMessage = "";
+  directors: IDirector[] = [];
 
+  constructor(
+    private dadesFilmsService: DadesFilmsService,
+    private router: Router,
+    private formBuilder: FormBuilder,
+    private dadesDirectorsService: DadesDirectorsService,
+    private activatedRoute: ActivatedRoute) {
+    this.myForm = new FormGroup({})
+  }
+
+  ngOnInit(): void {
+    this.id = this.activatedRoute.snapshot.paramMap.get('id');
+
+    this.dadesFilmsService.getFilm(this.id).subscribe({
+      next: (data: any) => {
+        const film = data.body;
+        this.myForm.setValue({
+          title: film.title,
+          dataP: film.dataP,
+          duration: film.duration,
+          director_id: film.director_id
+          // fem el mateix per la resta de camps
+        });
+      },
+      error: (error: any) => {
+        this.errorMessage = error.message;
+      }
+    });
+
+    this.dadesDirectorsService.getDades().subscribe({
+      next: (response: HttpResponse<IDirector[]>) => {
+        this.directors = response.body || [];
+      },
+      error: (error) => {
+        this.errorMessage = error.message;
+      }
+    })
+
+    this.myForm = this.formBuilder.group({
+      title: [null],
+      dataP: [null],
+      duration: [null],
+      director_id: [null]
+    });
+  }
+
+  /**
+   * onSubmit
+   */
+  public onSubmit(film: any) {
+    this.dadesFilmsService.editFilm(this.id, film).pipe(
+      catchError(error => {
+        this.errorMessage = error.message;
+        return of(null);
+      })
+    ).subscribe({
+      next: (data) => {
+        if (data) {
+          this.router.navigateByUrl('/film-list');
+        }
+      }
+    });
+  }
 }
